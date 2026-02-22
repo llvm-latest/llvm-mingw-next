1101d6611613af86352d8d39001ae65dbc6bcc5c
Re <reaperofblack@gmail.com>
2026-02-22 22:48:03 +0800

    ci: remove redundant ccache installation on Linux and macOS

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -37,7 +37,7 @@ sudo apt-get install -y -o Dpkg::Use-Pty=0 apt-fast
 sudo apt-fast install -y -o Dpkg::Use-Pty=0 \
     wget curl software-properties-common \
     git git-lfs \
-    build-essential binutils binutils-dev ccache \
+    build-essential binutils binutils-dev \
     clang lld llvm gcc g++ make cmake ninja-build \
     python3 python3-pip nasm \
     autoconf automake autopoint libtool pkg-config \
